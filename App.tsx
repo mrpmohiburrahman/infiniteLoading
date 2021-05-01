2a4279f09b537800ffcc83c6cb14516e7793cfba
mrpmohiburrahman <35300157+mrpmohiburrahman@users.noreply.github.com>
2021-05-02 00:20:27 +0600

    add textInput on App.tsx

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import TextInput from './src/component/TextInput';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <TextInput 
+        placeHolder='type your text'
+        onChangeText={()=>console.log("you clicked me.")}
+      />
     </View>
   );
 }
